da3561bfc622f6cb27b59f2f848d972ef1eadf4b
keanuWheeze <48886873+justarandomguyintheinternet@users.noreply.github.com>
2022-05-06 10:24:38 +0200

    Update xmake.lua

--- src/xmake.lua
+++ src/xmake.lua
@@ -2,8 +2,11 @@ set_languages("cxx20")
 add_rules("mode.debug", "mode.release")
 
 add_cxflags("/bigobj", "/MP")
+add_defines("UNICODE")
 
 target("test")
+    add_defines("WIN32_LEAN_AND_MEAN", "NOMINMAX", "WINVER=0x0601")
     set_kind("shared")
     add_files("*.cpp")
     set_filename("radioExt.asi")
+    add_syslinks("User32", "Version")
