6f1daf62818f6467a2fc5a230938370451259a3b
keanuWheeze <48886873+justarandomguyintheinternet@users.noreply.github.com>
2023-08-06 01:06:44 +0200

    Unfinished restructuring

---
+++ modules/vehicle/radioManagerV.lua
@@ -0,0 +1,91 @@
+local Cron   = require("modules/utils/Cron")
+
+local managerV = {}
+
+function managerV:new(radioMod)
+	local o = {}
+
+    o.rm = radioMod
+    o.radios = {}
+    o.initialized = false
+    o.isMounted = false
+
+	self.__index = self
+   	return setmetatable(o, self)
+end
+
+function managerV:getRadioByName(name)
+    for _, radio in pairs(self.radios) do
+        if name == radio.name then
+            return radio
+        end
+    end
+
+    return nil
+end
+
+function managerV:switchToRadio(radio) -- Set avtiveRadio var to the radio object
+    if radio.active then return end
+    self:disableCustomRadio()
+    Cron.After(0.1, function()
+        if GetMountedVehicle(GetPlayer()) then
+            GetMountedVehicle(GetPlayer()):GetBlackboard():SetBool(GetAllBlackboardDefs().Vehicle.VehRadioState, true)
+            GetMountedVehicle(GetPlayer()):GetBlackboard():SetName(GetAllBlackboardDefs().Vehicle.VehRadioStationName, radio.name)
+        end
+        radio:activate()
+    end)
+end
+
+function managerV:disableCustomRadio() -- Just stop playback
+    for _, radio in pairs(self.radios) do
+        radio:deactivate()
+    end
+end
+
+function managerV:update()
+    local veh = GetMountedVehicle(GetPlayer())
+    if veh then
+        self.isMounted = true
+        if veh:IsEngineTurnedOn() then
+            local name = veh:GetBlackboard():GetName(GetAllBlackboardDefs().Vehicle.VehRadioStationName)
+            local radio = self:getRadioByName(name.value)
+
+            if radio and not radio.active then
+                radio:activate()
+            end
+        end
+    elseif self.isMounted then
+        self.isMounted = false
+        self:disableCustomRadio()
+    end
+end
+
+function managerV:handleMenu()
+    local veh = GetMountedVehicle(GetPlayer())
+    if not veh then return end
+
+    local name = veh:GetBlackboard():GetName(GetAllBlackboardDefs().Vehicle.VehRadioStationName)
+    local radio = self:getRadioByName(name.value)
+
+    if radio then
+        if radio.active then
+            radio:deactivate()
+        end
+    end
+end
+
+function managerV:handleTS() -- trainSystem comp
+    if self.rm.runtimeData.ts then
+        if not self.rm.runtimeData.ts.stationSys then return end
+        local train = self.rm.runtimeData.ts.stationSys.activeTrain
+        if train and train.playerMounted then
+            for _, radio in pairs(self.radios) do
+                if radio.active then
+                    GetMountedVehicle(GetPlayer()):ToggleRadioReceiver(false)
+                end
+            end
+        end
+    end
+end
+
+return managerV
